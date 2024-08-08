e7a64ba4665f507c9d13216fe1ca19ef3e595705
jean0t <joaomouryf@hotmail.com>
2024-08-07 23:27:16 -0300

    parser development and tests included

--- parser/parser.go
+++ parser/parser.go
@@ -13,4 +13,20 @@ type Parser struct {
 	peekToken token.Token
 }
 
-// more
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
